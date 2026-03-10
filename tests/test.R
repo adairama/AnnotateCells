cb0564041cd93b0c4ad00d90c064e33cb6a5fed6
Adaikalavan Ramasamy <adai@gis.a-star.edu.sg>
2026-03-11 03:54:01 +0700

    Updated the README.Rmd and knitted the md.

--- tests/test.R
+++ tests/test.R
@@ -1,16 +1,19 @@
-if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+# if (!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
+#
+# pacman::p_load( Seurat, SingleR, celldex )
+#
+# pacman::p_load_gh("prabhakarlab/RCAv2")
+# pacman::p_load_gh("lmw123/DISCOtoolkit")
+# pacman::p_load_gh("satijalab/Azimuth")
+# pacman::p_load_gh("adairama/AnnotateCells")
 
-pacman::p_load( Seurat, SingleR, celldex )
-
-pacman::p_load_gh("prabhakarlab/RCAv2")
-pacman::p_load_gh("lmw123/DISCOtoolkit")
-pacman::p_load_gh("satijalab/Azimuth")
-pacman::p_load_gh("adairama/AnnotateCells")
 
 
+# Setup -------------------------------------------------------------------
 pacman::p_load(tidyverse, Seurat, AnnotateCells)
 
 
+# Load data ---------------------------------------------------------------
 data(pbmc.demo)
 pbmc.demo@meta.data %>% head()
 
@@ -18,3 +21,9 @@ pred <- AnnotateCells(pbmc.demo, "RCAv2.GlobalPanel_CellTypes")
 
 pred <- AnnotateCells(pbmc.demo, "DISCO.all")
 
+
+
+
+pbmc.demo@meta.data %>%
+  count(seurat_annotations, RCAv2.GlobalPanel_CellTypes) %>%
+  arrange(seurat_annotations, desc(n))
