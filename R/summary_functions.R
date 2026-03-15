2157dff4e1a8fae5f5add2ca1d5f3ff3c36b4d35
Adaikalavan Ramasamy <adai@gis.a-star.edu.sg>
2026-03-15 15:15:17 +0800

    Updated align_prediction_to_cluster() with Nigel's suggested function. This avoids calculating the midpoint and uses position_stack(vjust=0.5).

--- R/summary_functions.R
+++ R/summary_functions.R
@@ -15,30 +15,30 @@
 #' @export
 #'
 #' @examples
+#'
+#'
+#'
 align_prediction_to_cluster <- function(prediction,
                                         cluster,
                                         thres = 0.70,
                                         type = c("plot", "split", "long", "long.all"),
                                         text.size = 2){
-
   type <- match.arg(type)
-  if( !between(thres, 0, 1) ) stop("`thres` should be between 0 and 1.")
 
-  ## Count
+  if(!(thres >= 0 & thres <= 1)) stop("`thres` should be between 0 and 1.")
+
   cts <- data.frame(prediction, cluster) %>%
-    dplyr::count(cluster, prediction) %>%
+    count(cluster, prediction) %>%
     arrange(cluster, desc(n)) %>%
     group_by(cluster) %>%
-    mutate(prop    = n / sum(n),
+    mutate(prop    = n/sum(n),
            cumprop = cumsum(prop))
 
-  if(type == "long.all") return(cts)
+  if (type == "long.all") return(cts)
 
-  # Select top predictions that cumulatively explain > thres amount
   top_prediction <- cts %>%
     filter(cumprop <= thres | dplyr::lag(cumprop, default = 0) < thres)
 
-  # Add the misc category
   misc <- anti_join(cts,
                     top_prediction %>% select(cluster, prediction),
                     by = c("cluster", "prediction")) %>%
@@ -50,36 +50,39 @@ align_prediction_to_cluster <- function(prediction,
 
   rm(cts, top_prediction, misc)
 
-  if(type == "long")  return(out)
+  ## Return outputs
+  if (type == "long") return(out)
 
-  if(type == "split") return(split(out %>% as.data.frame(), out$cluster))
+  if (type == "split") return(split(out %>% as.data.frame(), out$cluster))
 
-  ## Barplot visualization
-  if(type == "plot"){
+  if (type == "plot") {
 
-    ## rename cluster to include sample size
     tb <- tapply(out$n, out$cluster, sum)
+
     out$cluster2 <- plyr::mapvalues(out$cluster,
                                     from = names(tb),
-                                    paste0( names(tb), " (n=", tb, ")" ))
+                                    paste0(names(tb), " (n=", tb, ")"))
 
-    ## midpoint for labels
-    out <- mutate(out, cumprop.pos = cumprop - prop/2)
-    out$cumprop.pos[ which(out$prediction == "Misc.") ] <- NA
-
-    g <- ggplot(out, aes(y = cluster2, fill = prediction, weight = prop)) +
+    g <- ggplot(out, aes(x = prop, y = cluster2, fill = prediction)) +
       geom_vline(xintercept = thres) +
-      geom_bar(position = position_stack(reverse = TRUE)) +
+      geom_col(position = position_stack(reverse = TRUE)) +
       scale_y_discrete(limits = rev) +
-      geom_text(aes(x = cumprop.pos, label = prediction), size = text.size) +
-      scale_x_continuous(labels = scales::percent_format(),
-                         expand = expansion(mult = c(0, 0.01))) +
+      geom_text(
+        aes(label = ifelse(prediction == "Misc.", NA, prediction)),
+        position = position_stack(vjust = 0.5, reverse = TRUE),
+        size = text.size
+      ) +
+      scale_x_continuous(
+        labels = scales::percent_format(),
+        expand = expansion(mult = c(0, 0.01))
+      ) +
       labs(x = NULL, y = NULL) +
       theme_minimal() +
-      theme(legend.position = "none",
-            axis.text = element_text(size=12, face="bold"))
+      theme(
+        legend.position = "none",
+        axis.text = element_text(size = 12, face = "bold")
+      )
 
-    # Extract color mapping and set Misc. to grey
     cols <- ggplot_build(g)$data[[3]] %>%
       distinct(label, fill) %>%
       deframe()
